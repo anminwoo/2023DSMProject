1051105d6a1e96c686210e4e3b518026fa6f9470
GravityCiphers <114645542+GravityCiphers@users.noreply.github.com>
2023-07-04 16:12:44 +0900

    Update Player.cs

--- 2D_Project/Assets/Scripts_ojy/Player/Player.cs
+++ 2D_Project/Assets/Scripts_ojy/Player/Player.cs
@@ -15,6 +15,7 @@ namespace Player
         void Awake()
         {
             _rigid = GetComponent<Rigidbody2D>();
+            _spriter = GetComponent<SpriteRenderer>();
         }
         void Update()
         {
@@ -37,7 +38,7 @@ namespace Player
         {
             if (inputVector.x != 0)
             {
-                
+                _spriter.flipX = inputVector.x < 0;
             }
         }
         public void OnDamage()
