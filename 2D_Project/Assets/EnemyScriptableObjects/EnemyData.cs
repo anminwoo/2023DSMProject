5b172b76714d75c4cfa313d3592f34e258929f29
zion <jd060625@naver.com>
2023-07-04 15:38:08 +0900

    EnemyData edit

--- 2D_Project/Assets/EnemyScriptableObjects/EnemyData.cs
+++ 2D_Project/Assets/EnemyScriptableObjects/EnemyData.cs
@@ -11,11 +11,6 @@ public class EnemyData : ScriptableObject
     public float speed;
     public float searchRange;
     
-<<<<<<< HEAD
     [Header("Animator")]
     public RuntimeAnimatorController controller;
-=======
-    [Header("Monster Type")]
-    [SerializeField] Animator animator;
->>>>>>> main
 }
