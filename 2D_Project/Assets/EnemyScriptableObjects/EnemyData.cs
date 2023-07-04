fad3090b7d5e85dc48933f11c50c8b75fa955b2e
zion <jd060625@naver.com>
2023-07-04 13:03:57 +0900

    ScriptableObject, Sprite

---
+++ 2D_Project/Assets/EnemyScriptableObjects/EnemyData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "enemyData", menuName = "Scriptable Object/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    [Header("Stat")]
+    [SerializeField] int hp;
+    [SerializeField] float attackCd;
+    [SerializeField] private int damage;
+    [SerializeField] private float speed;
+    
+    [Header("Monster Type")]
+    [SerializeField] Sprite[] type;
+}
