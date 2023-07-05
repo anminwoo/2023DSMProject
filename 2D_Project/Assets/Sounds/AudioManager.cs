1f10272c4d980a3ef5368e58e6f690a6b7e15648
GravityCiphers <114645542+GravityCiphers@users.noreply.github.com>
2023-07-05 15:22:21 +0900

    Create Sfx and Bgm

--- 2D_Project/Assets/Sounds/AudioManager.cs
+++ 2D_Project/Assets/Sounds/AudioManager.cs
@@ -11,6 +11,7 @@ public class AudioManager : MonoBehaviour
     public AudioClip bgmClip;
     public float bgmVolume;
     AudioSource bgmPlayer;
+    AudioHighPassFilter bgmEffect;
     
     [Header("#SFX")]
     public AudioClip[] sfxClips;
@@ -18,6 +19,25 @@ public class AudioManager : MonoBehaviour
     public int channels;
     AudioSource[] sfxPlayers;
     int channelIndex;
+
+    public enum Sfx
+    {
+        Chepen, // 상자 열 때 
+        MonDam, // 몬스터가 피해를 입을 때
+        Doorpen, // 문열 때 
+        Plattack, // 플레이어가 공격할 때
+        Plaking, // 플래이어가 걸을 때
+        Plath, // 플레이어가 죽을 때
+        PlaDam, // 플례이가 데미지를 입을 때 
+        Skeltakk, // 스켈레톤이 공격할 때
+        SkelDie, // 스켈레톤이 죽을 때
+        Spidttack, // 거미가 공격할 때
+        SpidDie, // 거미가 죽을 때
+        Zomttack, // 좀비가 공겨할 때
+        ZomDie, // 좀비가 죽을 때
+        BossAppear // 보스가 등장할 때
+    };
+    
     void Awake()
     {
         instance = this;
@@ -34,7 +54,8 @@ public class AudioManager : MonoBehaviour
         bgmPlayer.loop = true;
         bgmPlayer.volume = bgmVolume;
         bgmPlayer.clip = bgmClip;
-
+        bgmEffect = Camera.main.GetComponent<AudioHighPassFilter>();
+        
         // 효과음 플레이어 초기화
         GameObject sfxObject = new GameObject("SfxPlayer");
         sfxObject.transform.parent = transform;
@@ -44,8 +65,44 @@ public class AudioManager : MonoBehaviour
         {
             sfxPlayers[index] = sfxObject.AddComponent<AudioSource>();
             sfxPlayers[index].playOnAwake = false;
+            sfxPlayers[index].bypassListenerEffects = true;
             sfxPlayers[index].volume = sfxVolume;
         }
         
+    }
+    
+
+    public void PlayBgm(bool isPlay) 
+    {
+        if (isPlay)
+        {
+            bgmPlayer.Play();
+        }
+        else {
+            bgmPlayer.Stop();
+        }
+    }
+    
+    
+    public void EffectBgm(bool isPlay)
+    {
+        bgmEffect.enabled = isPlay;
+    }
+
+    public void playSfx(Sfx sfx)
+    {
+        for (int index = 0; index < sfxPlayers.Length; index++)
+        {
+            int loopIndex = (index + channelIndex) % sfxPlayers.Length;
+            if (sfxPlayers[loopIndex].isPlaying)
+            {
+                continue;
+            }
+
+            channelIndex = loopIndex;
+            sfxPlayers[0].clip = sfxClips[(int)sfx];
+            sfxPlayers[0].Play();
+            break;
+        }
     }
 }
