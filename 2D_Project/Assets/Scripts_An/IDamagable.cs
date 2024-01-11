058e922125883b88eaa08496413cc9f8ec443113
Gravity <114645542+GravityCiphers@users.noreply.github.com>
2024-01-11 22:16:58 +0900

    Delete 2D_Project directory

--- 2D_Project/Assets/Scripts_An/IDamagable.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDamagable
-{
-    public void GetDamage(int damage);
-}
--- 2D_Project/Assets/Scripts_An/IInteractable.cs
+++
@@ -1,5 +0,0 @@
-﻿
-public interface IInteractable
-{
-    public void Interact();
-}
